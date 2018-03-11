1372ad17bd48b29d680280f70dfc1ce90cfd1cab
zhongdeming428 <570160841@qq.com>
2018-03-11 17:42:19 +0800

    update 2018.03.11

--- UnderscoreSourceCode/test.js
+++ UnderscoreSourceCode/test.js
@@ -2,33 +2,33 @@
 
 const _ = require('./underscore.js');
 
-var a = {
-    name: 'test'
-};
-a['test1'] = a;
-var b = {
-    name: 'test'
-};
-b['test1'] = b;
+// var a = {
+//     name: 'test'
+// };
+// a['test1'] = a;
+// var b = {
+//     name: 'test'
+// };
+// b['test1'] = b;
 
 // console.log(_.isEqual(a, b));
 
-var set1 = new Set([1,2,3,4]);
-var set2 = new Set([1,2,3]);
+// var set1 = new Set([1,2,3,4]);
+// var set2 = new Set([1,2,3]);
 // console.log(_.isEqual(set1, set2));
 // console.log(Array.prototype.slice.call(set1.values(), 0));
 // console.log(set1.keys());
 
-var arr1 = [['name', 'zdm'], ['age', 22]];
-var arr2 = [['name', 'zdm'], ['age', 22], ['gender', 'male']];
-var map1 = new Map(arr1);
-var map2 = new Map(arr2);
+// var arr1 = [['name', 'zdm'], ['age', 22]];
+// var arr2 = [['name', 'zdm'], ['age', 22], ['gender', 'male']];
+// var map1 = new Map(arr1);
+// var map2 = new Map(arr2);
 
 // console.log(_.isEqual(map1, map2));
 // console.log([...map2]);
 
-var x = /.\w+/gi;
-var y = /.\w+/ig;
+// var x = /.\w+/gi;
+// var y = /.\w+/ig;
 
 // console.log(_.isEqual(x, y));
 
@@ -79,29 +79,72 @@ var y = /.\w+/ig;
 //     a();
 // }
 
-var throttle = function(func, wait){
-    var timeout, result, now;
-    var previous = 0;
+// var throttle = function(func, wait){
+//     var timeout, result, now;
+//     var previous = 0;
     
-    return function() {
-        now = +(new Date());
+//     return function() {
+//         now = +(new Date());
     
-        if(now - previous >= wait) {
-            if(timeout) {
-                clearTimeout(timeout);
-                timeout = null;
-            }
-            previous = now;
-            result = func.apply(this, arguments);
-        }
-        else if(!timeout) {
-            timeout = setTimeout(function() {
-                previous = now;
-                result = func.apply(this, arguments);
-                timeout = null;
-            }, wait - now + previous);
-        }
-        return result;
-    }
-}
-window.onscroll = throttle(()=>{console.log('yes')}, 2000);
+//         if(now - previous >= wait) {
+//             if(timeout) {
+//                 clearTimeout(timeout);
+//                 timeout = null;
+//             }
+//             previous = now;
+//             result = func.apply(this, arguments);
+//         }
+//         else if(!timeout) {
+//             timeout = setTimeout(function() {
+//                 previous = now;
+//                 result = func.apply(this, arguments);
+//                 timeout = null;
+//             }, wait - now + previous);
+//         }
+//         return result;
+//     }
+// }
+// window.onscroll = throttle(()=>{console.log('yes')}, 2000);
+
+// var alert = ()=>{
+//     console.log('yes..');
+// }
+// var b = _.before(3, alert);
+// b();
+// b();
+// b();
+
+// var a = {
+//     name: 'test',
+//     sayName: function() {
+//         console.log(this.name);
+//     }
+// };
+// // _.bindAll(a, 'sayName');
+
+// var b = a.sayName;
+// name = 'window.test';
+// b();
+
+// function a(arr) {
+//     if(arguments.length < 2)
+//         throw new Error('funciton a require at least 2 parameters!');
+//     return arr.concat(Array.prototype.slice.call(arguments, 1));
+// }
+
+// console.log(a([1, 2], 3));
+
+// function a(arr, filter) {
+//     if(arguments.length < 3)
+//         throw new Error('function a require at least 3 parameters!');
+//     var params = Array.prototype.slice.call(arguments, 2);
+//     for(var i = 0; i < params.length; i++) {
+//         if(filter(params[i])) {
+//             arr.push(params[i]);
+//         }
+//     }
+//     return arr;
+// }
+// console.log(a([], (x) => {
+//     return x > 1;
+// }, 1, 2, 3, 0));
