460b719fcce5c50d730ed158b7a50b37ffab314e
zhongdeming428 <570160841@qq.com>
2018-07-03 18:59:08 +0800

    完成冒泡排序Python实现

--- SortExamples/PythonImplementation/BubbleSort.py
+++ SortExamples/PythonImplementation/BubbleSort.py
@@ -1,4 +1,12 @@
 from Global import generateList
 
 def bubbleSort(l):
-    
+    for index, item in enumerate(l):
+        for i in range(0, len(l) - index - 1):
+            if l[i] > l[i + 1]:
+                tmp = l[i]
+                l[i] = l[i + 1]
+                l[i + 1] = tmp
+    return l
+
+print(bubbleSort(generateList(10000)))
